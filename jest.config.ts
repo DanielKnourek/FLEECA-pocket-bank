8e0bc70e387b03a6623287dd5b18a11ebc9ffcba
Daniel Kňourek <knourek.daniel@gmail.com>
2023-03-01 19:48:41 +0100

    feat: src coverage threshold

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,5 @@
-import nextJest from "next/jest"
+import nextJest from "next/jest";
+import { Config } from "jest";
 
 const createJestConfig = nextJest({
     // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -6,12 +7,19 @@ const createJestConfig = nextJest({
 })
 
 // Add any custom config to be passed to Jest
-/** @type {import('jest').Config} */
-const customJestConfig = {
+const customJestConfig: Config = {
     // Add more setup options before each test is run
     // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
-    testEnvironment: 'jest-environment-jsdom',
+    collectCoverageFrom: ['./src/**'],
+    coverageThreshold: {
+        global: {
+            functions: 90,
+            lines: 80,
+        }
+    }
+
+
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
