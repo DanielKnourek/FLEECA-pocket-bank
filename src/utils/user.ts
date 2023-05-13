0b704eefe0fbde7f91a23aff0fe7874ee0a649b2
Daniel Kňourek <knourek.daniel@gmail.com>
2023-05-13 07:54:59 +0000

    feat: kysely pg ORM

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,29 +1,10 @@
-import { createClient, db, sql } from "@vercel/postgres";
-import { z } from "zod";
+import { sql } from "@vercel/postgres";
 
 
 const insertUser = async () => {
-    // const SQLenvSchema = z.object({
-    //     POSTGRES_DATABASE: z.string()
-    // });
-    const client = createClient();
-    await client.connect();
-    
-    const KeyMatcher = z.string().startsWith("POSTGRES").catch(() => {
-        return "null"
-    })
-    const SQLenvSchema = z.record(KeyMatcher, z.string())
-    // const SQLenvSchema = z.record(z.string(), z.string())
-    // SQLenvSchema.parse({
-    //     AA: "stringdata",
-    //     AAB: "stringdata",
-    //     B: "stringdata",
-    //     BBB: "stringdata",
-    // });
 
-    console.log("################ DB test! #########################");
-    console.log(`env: ${JSON.stringify(SQLenvSchema.parse(process.env), null, "\t")}`);
-    const result = await sql`INSERT INTO users (name, surname) VALUES ('Pan', 'Nar')`;
+    const result = await sql`SELECT FROM information_schema.tables;`;
+    // const result = await sql`INSERT INTO users (name, surname) VALUES ('Pan', 'Nar')`;
     console.log(result);
 }
 
