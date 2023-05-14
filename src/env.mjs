5f830d055dc92acf61fd0c2a553fa0dd35bc0b05
Daniel Kňourek <knourek.daniel@gmail.com>
2023-05-14 02:35:07 +0000

    fix: @t3-oss/env hack not needed anymore

--- src/env.mjs
+++ src/env.mjs
@@ -3,7 +3,6 @@ import { createEnv } from "@t3-oss/env-nextjs";
 
 export const env = createEnv({
   
-  isServer: process.env?.IS_TEST === "true" ? true : undefined,
   /**
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
