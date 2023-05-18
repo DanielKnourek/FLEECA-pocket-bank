9963e778b0398479a9a64ba8d166465a854ccb54
Daniel Kňourek <knourek.daniel@gmail.com>
2023-05-18 06:06:43 +0000

    feat: db selectfrom unittest

---
+++ __test__/server/db/index.test.ts
@@ -0,0 +1,28 @@
+import { db } from '@/server/db'
+import { Database } from '@/server/db/db-schema'
+import { InsertType, SelectType, Selectable, QueryResult } from 'kysely'
+import { fixtureUserAccount } from 'scripts/db/fixtures/db-schema';
+import { describe, it, expect, vi, expectTypeOf } from 'vitest'
+import { DeepMockProxy, mock, mockDeep } from 'vitest-mock-extended';
+describe('Validate db querry builder', () => {
+
+    it('select from {UserAccount}', async () => {
+        const fixtureResult: QueryResult<typeof fixtureUserAccount> = {
+            rows: [
+                fixtureUserAccount
+            ],
+        }
+        const compiledQuerry = db.selectFrom('UserAccount').selectAll().compile();
+        expect(compiledQuerry).toBeTruthy();
+
+        const mockDB: DeepMockProxy<typeof db> = mockDeep<typeof db>({ funcPropSupport: true });
+        mockDB.executeQuery.calledWith(compiledQuerry).mockReturnValue(Promise.resolve(fixtureResult));
+        const executedResult = (await mockDB.executeQuery(compiledQuerry)) as QueryResult<typeof fixtureUserAccount> ;
+
+        expect(executedResult.rows.length).toBeGreaterThan(0);
+        expect(executedResult.rows[0]?.email).eq(fixtureUserAccount.email);
+        
+
+    })
+
+})
