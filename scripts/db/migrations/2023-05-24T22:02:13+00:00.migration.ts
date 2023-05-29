f08ca4a1ddadc5436a703d8cc39ed2dfcf9807d2
Daniel Kňourek <knourek.daniel@gmail.com>
2023-05-29 13:04:19 +0000

    feat: registered user cannot register agin

---
+++ scripts/db/migrations/2023-05-24T22:02:13+00:00.migration.ts
@@ -0,0 +1,22 @@
+import { Kysely, sql } from 'kysely';
+import { CurrentSchema } from '../migrator';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  console.info(`Migration ${__filename} up on schema: ${CurrentSchema}`);
+  // Migration code
+
+  await db.schema.withSchema(CurrentSchema)
+    .alterTable('UserAccount')
+    .addUniqueConstraint('UserAccount_unique_email_provider_pair', ['email', 'login_platform_uid'])
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  console.info(`Migration ${__filename} down on schema: ${CurrentSchema}`);
+  // Migration code
+
+  await db.schema.withSchema(CurrentSchema)
+    .alterTable('UserAccount')
+    .dropConstraint('UserAccount_unique_email_provider_pair')
+    .execute();
+}
