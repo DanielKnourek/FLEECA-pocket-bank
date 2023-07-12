47cb9cd01d21b36c8550dbb22e1c9b670fe103ea
Daniel Kňourek <knourek.daniel@gmail.com>
2023-07-12 16:38:16 +0000

    feat: frontpage init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Layout from "@/components/Layout";
 
 
 const Home: NextPage = () => {
@@ -66,18 +67,8 @@ const AuthShowcase: React.FC = () => {
   const secretMessage = "fake secret"
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
+    <Layout requireSession={true}>
+      <h1> FLEECA pocket bank</h1>
+    </Layout>
   );
 };
